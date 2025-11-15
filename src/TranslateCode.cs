35aa2b77d626cd78385458fa1f37c50a6bb978e9
Codeleme Games <117938403+CodelemeGames@users.noreply.github.com>
2025-11-15 17:27:58 +0000

    Made code processing separate from the syntax data

--- src/TranslateCode.cs
+++ src/TranslateCode.cs
@@ -2,55 +2,25 @@ using System.Text.RegularExpressions;
 
 public class Translator
 {
-    Dictionary<string, string> Conversions = new Dictionary<string, string>()
-    {
-        { "vd", "void" },
-        { "in", "int" },
-        { "tdf", "typedef"},
-        { "dt", "delete" },
-        { "ptf", "printf"},
-        { "str", "struct"},
-        { "rt", "return"},
-        { "fl", "float"},
-        { "ch", "char"},
-        { "use", "using"},
-        { "name", "namespace"},
-        { "inc", "include"},
-        { "mut", "mutable" },
-        { "vec", "vector"},
-        { "conpr", "constexpr" },
-        { "conva", "consteval"},
-        { "conit", "constinit"},
-        { "cnst", "const" },
-        { "cls", "class"},
-    };
-
-    List<string> invalidSyntax = new List<string>()
-    {
-        { "-" },
-        { "*"},
-        { ">"}
-    };
-
-    Dictionary<string, string> replaceFunctions = new Dictionary<string, string>()
-    {
-        { "ptf", "printf, std::cout"},
-        { "inp", "scanf, std::cin"}
-    };
-
     bool isToC = false;
+
+    string[] dataParsing(string path)
+    {
+        string data = File.ReadAllText(path);
+        string[] lines = data.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        return lines;
+    }
 
     public void ProcessData(string? path, string? language)
     {
         isToC = (language?.ToLower() == "c") ? true : false;
-
         int errors = 0;
 
         if (path == null || !path.Contains(".particle") || !File.Exists(path))
             return;
 
-        string data = File.ReadAllText(path);
-        string[] lines = data.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        string[] lines = dataParsing(path);
 
         for (int i = 0; i < lines.Length; i++)
         {
@@ -60,7 +30,7 @@ public class Translator
                 Console.WriteLine(lines[i]);
             }
 
-            foreach (var syntax in Conversions)
+            foreach (var syntax in TranspilationData.Conversions)
             {
                 lines[i] = Regex.Replace(lines[i], @$"\b{syntax.Key}\b", syntax.Value);
                 lines[i] = Regex.Replace(lines[i], @"->", ".");
@@ -110,20 +80,5 @@ public class Translator
         }
 
         return false;
-    }
-
-    void functionReplacements(string line)
-    {
-        foreach (var syntax in replaceFunctions)
-        {
-            if (line.Contains(syntax.Key))
-                goto hasSyntax;
-        }
-
-    hasSyntax:
-        if (!isToC)
-        {
-            
-        }
     }
 }
---
+++ src/TranspilationData.cs
@@ -0,0 +1,31 @@
+public class TranspilationData
+{
+    public static Dictionary<string, string> Conversions = new Dictionary<string, string>()
+    {
+        { "vd", "void" },
+        { "in", "int" },
+        { "tdf", "typedef"},
+        { "dt", "delete" },
+        { "ptf", "printf"},
+        { "str", "struct"},
+        { "rt", "return"},
+        { "fl", "float"},
+        { "ch", "char"},
+        { "use", "using"},
+        { "name", "namespace"},
+        { "inc", "include"},
+        { "mut", "mutable" },
+        { "vec", "vector"},
+        { "conpr", "constexpr" },
+        { "conva", "consteval"},
+        { "conit", "constinit"},
+        { "cnst", "const" },
+    };
+
+    public static List<string> invalidSyntax = new List<string>()
+    {
+        { "-" },
+        { "*"},
+        { ">"}
+    };
+};
